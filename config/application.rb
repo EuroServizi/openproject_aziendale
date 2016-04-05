8d3d214f18b1a11747ba3f7bf2185e9d0def80ad
fabiano <fabiano.pavan@soluzionipa.it>
2016-04-05 10:56:46 +0200

    Aggiunto locale italiano con formati date corrette

--- config/application.rb
+++ config/application.rb
@@ -103,7 +103,10 @@ module OpenProject
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    #config.i18n.default_locale = :en
+    #personalizzo formati in italiano
+    config.i18n.default_locale = :it
+
 
     # Fall back to default locale
     config.i18n.fallbacks = true
