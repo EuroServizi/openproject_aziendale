b01742229379b2bf003ded2c34a9d4ebd9b2942f
Fabiano <fabiano.pavan@soluzionipa.it>
2018-05-15 14:31:19 +0200

    Aggiunto calcolo ore e aggiornato uglify_js

---
+++ OPworker.sh
@@ -0,0 +1,3 @@
+su - openproject
+cd /home/openproject/openproject-ce
+RAILS_ENV=production /home/openproject/.rvm/gems/ruby-2.1.2/bin/bundle exec /home/openproject/.rvm/gems/ruby-2.1.2/wrappers/rake jobs:workoff
